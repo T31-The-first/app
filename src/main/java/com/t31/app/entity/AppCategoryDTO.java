dd07c2acf4630596fdce0df0caf23d08ce09b45d
余学茂 <2630177132@qq.com>
2019-12-13 22:14:21 +0800

    1.0
    appstore第一版，实体类和资源配置完成

---
+++ src/main/java/com/t31/app/entity/AppCategoryDTO.java
@@ -0,0 +1,92 @@
+package com.t31.app.entity;
+
+import java.util.Date;
+
+public class AppCategoryDTO {
+        private int id;
+        private String categoryCode;
+        private String categoryName;
+        private int parentId;
+        private int createdBy;
+        private Date creationTime;
+        private int modifyBy;
+        private Date modifyDate;
+
+    @Override
+    public String toString() {
+        return "AppCategoryDTO{" +
+                "id=" + id +
+                ", categoryCode='" + categoryCode + '\'' +
+                ", categoryName='" + categoryName + '\'' +
+                ", parentId=" + parentId +
+                ", createdBy=" + createdBy +
+                ", creationTime=" + creationTime +
+                ", modifyBy=" + modifyBy +
+                ", modifyDate=" + modifyDate +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(String categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Date creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public int getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(int modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+}
---
+++ src/main/java/com/t31/app/entity/AppInfoDTO.java
@@ -0,0 +1,242 @@
+package com.t31.app.entity;
+
+import java.util.Date;
+
+public class AppInfoDTO {
+        private int id;
+        private String softwareName;
+        private String APKName;
+        private String supportROM;
+        private String interfaceLanguage;
+        private double softwareSize;
+        private Date updateDate;
+        private int devId;
+        private String appInfo;
+        private int status;
+        private Date onSaleDate;
+        private Date offSaleDate;
+        private int flatformId;
+        private int categoryLevel3;
+        private int downloads;
+        private int createdBy;
+        private Date creationDate;
+        private int modifyBy;
+        private int categoryLevel1;
+        private int categoryLevel2;
+        private String logoPocPath;
+        private String logoLocPath;
+        private int versionId;
+
+    @Override
+    public String toString() {
+        return "AppInfoDTO{" +
+                "id=" + id +
+                ", softwareName='" + softwareName + '\'' +
+                ", APKName='" + APKName + '\'' +
+                ", supportROM='" + supportROM + '\'' +
+                ", interfaceLanguage='" + interfaceLanguage + '\'' +
+                ", softwareSize=" + softwareSize +
+                ", updateDate=" + updateDate +
+                ", devId=" + devId +
+                ", appInfo='" + appInfo + '\'' +
+                ", status=" + status +
+                ", onSaleDate=" + onSaleDate +
+                ", offSaleDate=" + offSaleDate +
+                ", flatformId=" + flatformId +
+                ", categoryLevel3=" + categoryLevel3 +
+                ", downloads=" + downloads +
+                ", createdBy=" + createdBy +
+                ", creationDate=" + creationDate +
+                ", modifyBy=" + modifyBy +
+                ", categoryLevel1=" + categoryLevel1 +
+                ", categoryLevel2=" + categoryLevel2 +
+                ", logoPocPath='" + logoPocPath + '\'' +
+                ", logoLocPath='" + logoLocPath + '\'' +
+                ", versionId=" + versionId +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSoftwareName() {
+        return softwareName;
+    }
+
+    public void setSoftwareName(String softwareName) {
+        this.softwareName = softwareName;
+    }
+
+    public String getAPKName() {
+        return APKName;
+    }
+
+    public void setAPKName(String APKName) {
+        this.APKName = APKName;
+    }
+
+    public String getSupportROM() {
+        return supportROM;
+    }
+
+    public void setSupportROM(String supportROM) {
+        this.supportROM = supportROM;
+    }
+
+    public String getInterfaceLanguage() {
+        return interfaceLanguage;
+    }
+
+    public void setInterfaceLanguage(String interfaceLanguage) {
+        this.interfaceLanguage = interfaceLanguage;
+    }
+
+    public double getSoftwareSize() {
+        return softwareSize;
+    }
+
+    public void setSoftwareSize(double softwareSize) {
+        this.softwareSize = softwareSize;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public int getDevId() {
+        return devId;
+    }
+
+    public void setDevId(int devId) {
+        this.devId = devId;
+    }
+
+    public String getAppInfo() {
+        return appInfo;
+    }
+
+    public void setAppInfo(String appInfo) {
+        this.appInfo = appInfo;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getOnSaleDate() {
+        return onSaleDate;
+    }
+
+    public void setOnSaleDate(Date onSaleDate) {
+        this.onSaleDate = onSaleDate;
+    }
+
+    public Date getOffSaleDate() {
+        return offSaleDate;
+    }
+
+    public void setOffSaleDate(Date offSaleDate) {
+        this.offSaleDate = offSaleDate;
+    }
+
+    public int getFlatformId() {
+        return flatformId;
+    }
+
+    public void setFlatformId(int flatformId) {
+        this.flatformId = flatformId;
+    }
+
+    public int getCategoryLevel3() {
+        return categoryLevel3;
+    }
+
+    public void setCategoryLevel3(int categoryLevel3) {
+        this.categoryLevel3 = categoryLevel3;
+    }
+
+    public int getDownloads() {
+        return downloads;
+    }
+
+    public void setDownloads(int downloads) {
+        this.downloads = downloads;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(int modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public int getCategoryLevel1() {
+        return categoryLevel1;
+    }
+
+    public void setCategoryLevel1(int categoryLevel1) {
+        this.categoryLevel1 = categoryLevel1;
+    }
+
+    public int getCategoryLevel2() {
+        return categoryLevel2;
+    }
+
+    public void setCategoryLevel2(int categoryLevel2) {
+        this.categoryLevel2 = categoryLevel2;
+    }
+
+    public String getLogoPocPath() {
+        return logoPocPath;
+    }
+
+    public void setLogoPocPath(String logoPocPath) {
+        this.logoPocPath = logoPocPath;
+    }
+
+    public String getLogoLocPath() {
+        return logoLocPath;
+    }
+
+    public void setLogoLocPath(String logoLocPath) {
+        this.logoLocPath = logoLocPath;
+    }
+
+    public int getVersionId() {
+        return versionId;
+    }
+
+    public void setVersionId(int versionId) {
+        this.versionId = versionId;
+    }
+}
