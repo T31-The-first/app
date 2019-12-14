edadbf63a582e7dda8d20df763eeea4a0b8da449
余学茂 <2630177132@qq.com>
2019-12-14 19:37:21 +0800

    实体类已经全部创建完毕，新增controller分层，由于这个项目有两个模块，所以分controller分两层，准备把service,dao也进行分层处理

--- src/main/java/com/t31/app/controller/developer/DeveloperUserController.java
+++ src/main/java/com/t31/app/controller/developer/DeveloperUserController.java
@@ -1,12 +1,15 @@
-package com.t31.app.controller;
+package com.t31.app.controller.developer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class LoginController {
-        @RequestMapping("/devlogin")
+public class DeveloperUserController {
+        @RequestMapping("/devlogin.html")
         public String devLogin(){
             return "devlogin";
+        }
+        public String index(){
+            return "main";
         }
 }
