95e7c605fae8f6d08b6223ba077b5d97970650d6
余学茂 <2630177132@qq.com>
2019-12-14 20:49:25 +0800

    实体类已经全部创建完毕，新增controller分层，由于这个项目有两个模块，所以分controller分两层，准备把service,dao也进行分层处理

---
+++ src/main/java/com/t31/app/controller/backend/BackendUserController.java
@@ -0,0 +1,10 @@
+package com.t31.app.controller.backend;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class BackendUserController {
+        @RequestMapping("/backendlogin.html")
+        public String backendLogin(){
+            return "backendlogin";
+        }
+}
---
+++ src/main/java/com/t31/app/entity/DevUserDTO.java
@@ -0,0 +1,112 @@
+package com.t31.app.entity;
+
+import java.util.Date;
+
+public class DevUserDTO {
+        private int id;
+        private String devCode;
+        private String devName;
+        private String devPassword;
+        private String devEmail;
+        private String devInfo;
+        private int createdBy;
+        private Date creationDate;
+        private int modifyBy;
+        private Date modifyDate;
+
+    @Override
+    public String toString() {
+        return "DevUserDTO{" +
+                "id=" + id +
+                ", devCode='" + devCode + '\'' +
+                ", devName='" + devName + '\'' +
+                ", devPassword='" + devPassword + '\'' +
+                ", devEmail='" + devEmail + '\'' +
+                ", devInfo='" + devInfo + '\'' +
+                ", createdBy=" + createdBy +
+                ", creationDate=" + creationDate +
+                ", modifyBy=" + modifyBy +
+                ", modifyDate=" + modifyDate +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDevCode() {
+        return devCode;
+    }
+
+    public void setDevCode(String devCode) {
+        this.devCode = devCode;
+    }
+
+    public String getDevName() {
+        return devName;
+    }
+
+    public void setDevName(String devName) {
+        this.devName = devName;
+    }
+
+    public String getDevPassword() {
+        return devPassword;
+    }
+
+    public void setDevPassword(String devPassword) {
+        this.devPassword = devPassword;
+    }
+
+    public String getDevEmail() {
+        return devEmail;
+    }
+
+    public void setDevEmail(String devEmail) {
+        this.devEmail = devEmail;
+    }
+
+    public String getDevInfo() {
+        return devInfo;
+    }
+
+    public void setDevInfo(String devInfo) {
+        this.devInfo = devInfo;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(int modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+}
