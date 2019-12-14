a6da9756014e5922e71c130896bf40f2f133d2e8
余学茂 <2630177132@qq.com>
2019-12-14 21:09:48 +0800

    1.1

--- src/main/java/com/t31/app/controller/developer/DeveloperUserController.java
+++ src/main/java/com/t31/app/controller/developer/DeveloperUserController.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/developer")
 public class DeveloperUserController {
         @RequestMapping("/devlogin.html")
         public String devLogin(){
             return "devlogin";
         }
+        @RequestMapping("/index.html")
         public String index(){
-            return "main";
+            return "developer/main";
         }
 }
