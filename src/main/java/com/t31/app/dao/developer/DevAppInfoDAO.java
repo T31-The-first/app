66c8e908b8c438f7413f0ae5766507b2098e7bb0
余学茂 <2630177132@qq.com>
2019-12-16 17:52:52 +0800

    1.5

---
+++ src/main/java/com/t31/app/dao/developer/DevAppInfoDAO.java
@@ -0,0 +1,12 @@
+package com.t31.app.dao.developer;
+
+import com.t31.app.entity.AppInfoDTO;
+import com.t31.app.entity.devinfo.AppInfoList;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface DevAppInfoDAO {
+    public List<AppInfoList> selectAppInfo(@Param("appinfo") AppInfoDTO appinfo,@Param("start") int start,@Param("size") int size);
+    public int selectAppCount(AppInfoDTO appinfo);
+}
---
+++ src/test/com/t31/app/dev/AppInfoTest.java
@@ -0,0 +1,26 @@
+package com.t31.app.dev;
+
+import com.t31.app.entity.devinfo.AppInfoList;
+import com.t31.app.service.developer.DevAppInfoService;
+import com.t31.app.util.Page;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class AppInfoTest {
+    @Autowired
+        private DevAppInfoService appInfoService;
+        @Test
+        public void selectAppInfoTest(){
+            Page page = new Page();
+            for(AppInfoList app:(List<AppInfoList>)appInfoService.selectAppInfo(null,page).getDataList()) {
+                System.out.println(app.getCategoryLevel1Name());
+            }
+        }
+}
