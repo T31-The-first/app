97914afe3f10d3417ef1cfad5cd549f5120ddf6c
余学茂 <2630177132@qq.com>
2019-12-16 14:23:15 +0800

    1.3

--- src/main/java/com/t31/app/controller/backend/BackendUserController.java
+++ src/main/java/com/t31/app/controller/backend/BackendUserController.java
@@ -1,13 +1,25 @@
 package com.t31.app.controller.backend;
 
+import com.t31.app.service.backend.BackendUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-@Controller
-@RequestMapping("/backend")
-public class BackendUserController {
-        @RequestMapping("/backendlogin.html")
-        public String backendLogin(){
-            return "backendlogin";
-        }
-}
 
+/**
+ * description: BackendUserControler
+ * date: 2019/12/16 13:43
+ * author: 周吉涛
+ * version: 1.0
+ */
+@Controller
+@RequestMapping("/backends")
+public class BackendUserController {
+    @Autowired
+    private BackendUserService userService;
+
+    @RequestMapping("/applist.html")
+    public String list(){
+
+        return "/backend/applist";
+    }
+}
