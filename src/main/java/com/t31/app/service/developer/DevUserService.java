d5b45bf0f3b2ed905b48ada904f3019be5efb712
余学茂 <2630177132@qq.com>
2019-12-16 14:18:59 +0800

    1.3

--- src/main/java/com/t31/app/service/developer/DevUserService.java
+++ src/main/java/com/t31/app/service/developer/DevUserService.java
@@ -1,4 +1,7 @@
 package com.t31.app.service.developer;
 
+import com.t31.app.entity.DevUserDTO;
+
 public interface DevUserService {
+        public DevUserDTO userLogin(String username,String password);
 }
--- src/main/java/com/t31/app/service/developer/impl/DevUserServiceImpl.java
+++ src/main/java/com/t31/app/service/developer/impl/DevUserServiceImpl.java
@@ -1,4 +1,17 @@
 package com.t31.app.service.developer.impl;
 
-public class DevUserServiceImpl {
+import com.t31.app.dao.developer.DevUserDAO;
+import com.t31.app.entity.DevUserDTO;
+import com.t31.app.service.developer.DevUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DevUserServiceImpl implements DevUserService {
+    @Autowired
+    private DevUserDAO devUserDAO;
+    @Override
+    public DevUserDTO userLogin(String username,String password) {
+        return devUserDAO.userLogin(username,password);
+    }
 }
